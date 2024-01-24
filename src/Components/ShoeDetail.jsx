52db2772fb8696f09e53c22a7e210d8ac41a40c5
Brian A <brian.ahlers@gmail.com>
2024-01-23 19:30:54 -0800

    add gradient bg to image

--- src/Components/ShoeDetail.jsx
+++ src/Components/ShoeDetail.jsx
@@ -3,17 +3,24 @@ import nike1 from "../assets/n1-min.png";
 export function ShoeDetail() {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
+        {/* SHOE IMAGE */}
       <div className="flex-1">
+        <div className="bg-gradient-to-br from-[#F637CF] from-25% via-[#E3D876] via-40% to-[#4DD4C6]">
         <img src={nike1} />
         </div>
+        </div>
       <div className="flex-1 space-y-6">
+        {/* SHOE TEXT DETAIL */}
         <div className="text-5xl font-black md:text-9xl">Nike Air Max 270</div>
         <div className="font-medium md:text-xl">
           {
             "THE NIKE AIR MAX 270 IS A LIFESTYLE SHOE THAT FITS YOUR LIFESTYLE. IT HAS TIGHT LOOKS"
           }
         </div>
-        <div className="text-3xl font-extrabold md:text-6xl">$100</div>
+        <div className="text-3xl font-extrabold md:text-6xl">
+            $100
+        </div>
+        {/* SHOE BUTTONS AND LINKS */}
         <div className="space-x-10">
           <button className="hover:bg-gray-800 active:bg-gray-700 bg-black text-white rounded-lg px-4 py-2 mt-4">
             Add to Cart
