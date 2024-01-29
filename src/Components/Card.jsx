bfef91c997327096f314f8510769465c7c80de32
Brian A <brian.ahlers@gmail.com>
2024-01-29 15:45:30 -0800

    card element set up

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,11 @@
 export function Card({ item }) {
   return (
-    <div className={`${item.className} cursor-pointer transition hover:scale-105`}>
-      <div>
-        <div>{item.title}</div>
-        <div>SHOP NOW +</div>
+    <div className={`${item.className} max-w-xl cursor-pointer transform transition hover:scale-105`}>
+      <div className="p-8">
+        <div className="text-2xl font-bold">{item.title}</div>
+        <div className="underline underline-offset-4 font-semibold mt-10">SHOP NOW +</div>
       </div>
-      <img src={item.src} alt="" />
+      <img className=" absolute top-0 left-[40%] h-40 w-56" src={item.src} alt="" />
     </div>
   );
 }
